277ff56545da13addb7f15e9f2112e3d8799a267
shaya <57603195+fairyaksh@users.noreply.github.com>
2020-05-03 23:43:47 +0100

    Carousel js
    automatic loop
    next/prev slide on button click
    internval or 4000s

--- js/index.js
+++ js/index.js
@@ -7,3 +7,25 @@ function smoothScroll() {
 function myFunction() {
   alert("Thank you for your stay!");
 }
+
+/*---------- CAROUSEL ----------*/
+
+var slides = document.querySelectorAll("#slides .slide");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 4000);
+
+/* Slides */
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = "slide";
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = "slide showing";
+}
